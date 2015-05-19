c1d5e6ce8ecfc999fb67b8cf87c9ef7da6478076
fmauricios <mauricioserf@hotmail.com>
2015-05-19 11:29:07 -0500

    Added gravatar by user

--- test/integration/custom_routes_test.rb
+++ test/integration/custom_routes_test.rb
@@ -10,9 +10,4 @@ class CustomRoutesTest < ActionDispatch::IntegrationTest
 		get '/register'
 		assert_response :success
 	end
-
-	# test "that a profile page works" do
-	# 	get '/fmauricios'
-	# 	assert_response :success
-	# end
 end
