544b290f849b82535803c654a7b1f903ecfa843e
fmauricios <mauricioserf@hotmail.com>
2015-05-22 22:43:34 -0500

    Added responds with html or json, adding ajaxfor user_friend and user_friendships

--- app/assets/javascripts/user_friendships.js
+++ app/assets/javascripts/user_friendships.js
@@ -1,17 +1,26 @@
+window.userFriendships = [];
+
 $(document).ready(function () {
+	$.ajax({
+		url: Routes.user_friendships_path({ format: 'json'}),
+		dataType: 'json',
+		type: 'GET',
+		success: function (data) {
+			window.userFriendships = data
+		}
+	})
 
 	$('#add-friendship').on('click', function (event) {
 		event.preventDefault()
 		var addFriendsgipBtn = $(this)
 		$.ajax({
-			url: Routes.user_friendships_path({user_friendship: friend_id: addFriendsgipBtn.data('friendId')})
+			url: Routes.user_friendships_path( { user_friendship: {friend_id: addFriendsgipBtn.data('friendId') } }),
 			dataType: 'json',
 			type: 'POST',
 			success: function(e) {
 				addFriendsgipBtn.hide()
-				$('#friend-status').html("<a href='' class='btn btn-success'> Friendship Requested </a>")
+				$('#friend-status').html("<a href='#' class='btn btn-success'> Friendship Requested </a>")
 			}
 		})
 	})
-
 })
